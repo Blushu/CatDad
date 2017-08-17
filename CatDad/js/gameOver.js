aa898ca50fc1f262949875142a4bb949afdc0c6d
Blushu <dwyer.m.john@gmail.com>
2017-08-17 16:32:54 -0500

    Update 2.0
    Added sidewalk and building graphics and physics

--- CatDad/js/gameOver.js
+++ CatDad/js/gameOver.js
@@ -1,1 +1,19 @@
-var gameOverState;
+var gameOverState = {
+    
+    create: function() {
+        // Game Over Text
+        var gameOverLabel = game.add.text(80, 80, 'GAME OVER...', { font: '50px Arial', fill: '#00FF00' } );
+        // Instructions to start
+        var startLabel = game.add.text(80, game.world.height-80, 'press the "W" key to restart', { font: '25px Arial', fill: '#ffffff' } );
+        
+        // define the "W" key
+        var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+        // when W is pressed, call the start function
+        wkey.onDown.addOnce(this.restart, this);        
+    },
+    
+    restart: function() {
+        game.state.start('menu');
+    },
+    
+};
--- CatDad/js/load.js
+++ CatDad/js/load.js
@@ -8,6 +8,14 @@ var loadState = {
         game.load.image('sky', 'assets/sky.png');
         game.load.image('ground', 'assets/platform.png');
         game.load.image('star', 'assets/star.png');
+        game.load.image('sideWalkTL', 'assets/Sidewalk/TopL.png');
+        game.load.image('sideWalkTR', 'assets/Sidewalk/TopR.png');
+        game.load.image('sideWalkBL', 'assets/Sidewalk/bottomL.png');
+        game.load.image('sideWalkBR', 'assets/Sidewalk/bottomR.png');
+        game.load.image('trashCan', 'assets/Objects/trashCan.png');
+        game.load.image('home', 'assets/Home.png');
+        game.load.image('homeLedge', 'assets/Ledges/homeLedge.png');
+        game.load.image('homeLedgeTop', 'assets/Ledges/homeLedgeTop.png');
         game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
         game.load.spritesheet('catDad', 'assets/catDad_Sheet.png', 32, 32);
         game.load.spritesheet('cat', 'assets/CatSprite_Sheet.png', 18, 19);
