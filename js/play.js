8082bc6acdc90e88de4d79d027c3b7dc9912b13b
John Dwyer <19336208+Blushu@users.noreply.github.com>
2023-03-30 13:48:55 -0500

    fixed some spacing

--- js/play.js
+++ js/play.js
@@ -14,7 +14,7 @@ var playState = {
         game.global.pizza_count = 10;
         game.global.gravity = 450;
     
-    /*** AUDIO ***/    
+        /*** AUDIO ***/    
         // Add Audio (nickname, volume 0-1, looping defaults false)
         music = game.add.audio('background1', 0.7, true);
         dadjumpsound = game.add.audio('dadjump');
@@ -26,7 +26,7 @@ var playState = {
         // Play background Music
         music.play();
         
-    /*** ENVIRONMENT ***/
+        /*** ENVIRONMENT ***/
         
         // Add background
         background = game.add.sprite(0, 0, 'sky');
@@ -65,10 +65,7 @@ var playState = {
             trashCan.body.immovable = true;
         }
 
-
-
-
-    /*** PLAYERS ***/
+        /*** PLAYERS ***/
         /*** DAD ***/
         dad = game.add.sprite( game.world.width*0.8 - 16, game.world.height - 150, 'catDad' );
         game.physics.arcade.enable(dad);
@@ -76,7 +73,6 @@ var playState = {
         dad.body.collideWorldBounds = true;
         dad.animations.add( 'right', [2,3,4,5,6,7,8,9], 10, true );
         dad.animations.add( 'left', [10,11,12,13,14,15,16,17], 10, true );
-        
         
         /*** CAT ***/
         cat = game.add.sprite( dad.body.x + 44, game.world.height - 150, 'cat' );
@@ -86,21 +82,14 @@ var playState = {
         cat.animations.add('right', [2,3,4], 10, true);
         cat.animations.add('left', [5,6,7], 10, true);
 
-
-        
-        
-
-    /*** COLLECTABLES ***/
+        /*** COLLECTABLES ***/
         /*** PIZZAS ***/
         pizzas = game.add.group();
         pizzas.enableBody = true;
         // create 12 pizzas evenly spaced apart
         for (i = 0; i < game.global.pizza_count; i++) {
             // create a pizza in the 'pizzas' group
-			var drop_height = (i == 2) ? (game.world.height/2) : (game.world.height * 0.9);
-			
-			console.log(drop_height);
-			
+			var drop_height = (i == 2) ? (game.world.height/2) : (game.world.height * 0.9);			
             var pizza = pizzas.create(i*130 + 10, drop_height, 'pizza');
             // give them gravity so they fall
             pizza.body.gravity.y = 80;
@@ -108,11 +97,7 @@ var playState = {
             pizza.body.bounce.y = ( 0.2 + ( Math.random()*0.2 ) );
         }
         
-        
-        
-        
-        
-    /*** ENEMIES ***/
+        /*** ENEMIES ***/
         /*** DOG ***/    
         dog = game.add.sprite(0, game.world.height - 150, 'baddie');
         game.physics.arcade.enable(dog);
@@ -121,52 +106,34 @@ var playState = {
         dog.animations.add('left', [0,1], 6, true);
         dog.animations.add('right', [2,3], 6, true);
 
-
-
-    /*** PLAYER DATA ***/
+        /*** PLAYER DATA ***/
         /*** SCORE ***/
         game.global.scoreText = game.add.text(16, 16, 'Pizza: 0/' + game.global.pizza_count, {fontSize: '32px', fill: '#000'});
         game.global.scoreText.fixedToCamera = true;
         game.global.scoreText.cameraOffset.setTo(16, 16);
     },
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     update: function() {   
         
-    /*** CHECK FOR WIN ***/
+        /*** CHECK FOR WIN ***/
         if (game.global.score === game.global.pizza_count) {
 			music.destroy();
 			game.state.start('win');
         }
         
-    /*** GENERAL COLLISIONS ***/
+        /*** GENERAL COLLISIONS ***/
         // Collide pizzas with platforms
         game.physics.arcade.collide(pizzas, bottomSidewalks);
         game.physics.arcade.collide(pizzas, homeLedges);
         game.physics.arcade.collide(pizzas, trashCans);
         // Collide dog with sidewalk
-        game.physics.arcade.collide(dog, bottomSidewalks);
+        game.physics.arcade.collide(dog, bottomSidewalks);        
         
-        
-        
-        
-        
-        
-        
-    /*** INPUT ***/
+        /*** INPUT ***/
         cursors = game.input.keyboard.createCursorKeys(); //movement
         spacebarKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR); //switch character
         
-    /*** SWITCH FUNCTIONALITY ***/
+        /*** SWITCH FUNCTIONALITY ***/
         if (spacebarKey.isUp) {game.global.controlSwitchable = true} //allow one switch per key press
         
         // Check for switchability
@@ -196,8 +163,7 @@ var playState = {
                 break;
         }
         
-        
-    /*** CONTROLS ***/
+        /*** CONTROLS ***/
         function controls (leader, follower) {
             
             // collide with platforms, variables used for jumping
@@ -209,7 +175,7 @@ var playState = {
             
             leader.body.velocity.x = 0;
             
-        // MOVEMENT L & R
+            // MOVEMENT L & R
             if (cursors.left.isDown) {
                 switch (leader) {
                     case dad: leader.body.velocity.x = -220; break;
@@ -237,7 +203,7 @@ var playState = {
              // measure distance between Follower and the Leader
             var distanceFromLeader = ( follower.body.x - leader.body.x );
 
-        // FOLLOW THE LEADER
+            // FOLLOW THE LEADER
             if (cursors.left.isDown && (distanceFromLeader > (leader.body.width + 10) ) ) {
                 follower.animations.play('left');
                 switch (leader) {
@@ -252,6 +218,7 @@ var playState = {
                     case cat: follower.body.velocity.x = 170;              
                 }
             }
+
             // FACE THE LEADER
             else {
                 follower.animations.stop();
@@ -264,7 +231,7 @@ var playState = {
                 }
             }
             
-        // JUMP LOGIC 
+            // JUMP LOGIC 
             if (cursors.up.isUp) { game.global.jumped = false; } // allow one jump per key press
             if (cursors.up.isDown && leader.body.touching.down && (hitsideWalk || hittrashCan || hithomeLedge) && game.global.jumped == false) {
                 game.global.jumped = true;
@@ -280,9 +247,7 @@ var playState = {
                 }
             }
             
-           
-            
-        // check for pizza collision with the leader, if found, pass leader and pizza to the 'collectPizza' function
+            // check for pizza collision with the leader, if found, pass leader and pizza to the 'collectPizza' function
             game.physics.arcade.overlap(leader, pizzas, collectPizza, null, this);
 
             function collectPizza (leader, pizza) {
@@ -295,7 +260,7 @@ var playState = {
                 game.global.scoreText.text = 'Pizza: ' + game.global.score + '/' + game.global.pizza_count;
             }
             
-        /*** DOG MOVEMENT ***/
+            /*** DOG MOVEMENT ***/
             if ( dog.body.x == 0 && dog.body.touching.down ) {
                 dog.body.velocity.x = 100;
                 dog.animations.play('right');
@@ -304,7 +269,7 @@ var playState = {
                 dog.animations.play('left');
             }
             
-        /*** CHECK FOR GAMEOVER ***/
+            /*** CHECK FOR GAMEOVER ***/
             // Check for dog/leader collision, if found, pass leader and dog to gameOver function
             game.physics.arcade.overlap(leader, dog, gameOver, null, this);
 
@@ -312,11 +277,7 @@ var playState = {
                 music.destroy();
                 game.state.start('gameOver');
             }
-
-            
-            
         }
-        
     },
     
 /*
@@ -324,6 +285,5 @@ var playState = {
         
     },  
 */
-
     
 };
